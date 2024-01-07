2ce49ff72e24a6450aa4ab276ab703c7d1124659
ganesh-7985 <shankarganesh3005@gmail.com>
2024-01-07 20:22:42 +0530

    added all components with changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,39 +1,82 @@
-import React from 'react'
-import { RxHamburgerMenu } from "react-icons/rx";
-import { FaYoutube, FaMicrophone, FaRegBell } from "react-icons/fa";
-import { BiVideoPlus } from "react-icons/bi";
-import { CgProfile } from "react-icons/cg";
-import { IoIosSearch } from "react-icons/io";
+import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { BsYoutube } from "react-icons/bs";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMicrophone } from "react-icons/fa";
+import { RiVideoAddLine } from "react-icons/ri";
+import { BsBell } from "react-icons/bs";
+import { useLocation, useNavigate } from "react-router-dom";
+import { getSearchPageVideos } from "../store/reducers/getSearchPageVideos";
+import { useAppDispatch, useAppSelector } from "../hooks/useApp";
+import { changeSearchTerm, clearVideos } from "../features/youtube/youtubeSlice";
 
-function Navbar() {
-    return (
-        <div className='flex justify-between items-center flex-wrap bg-[#0f0f0f] px-14 sticky h-14'>
-            <div className='flex items-center gap-8 text-2xl text-white'>
-                <div><RxHamburgerMenu /></div>
-                <div className='flex items-center flex-wrap gap-2 justify-center '><FaYoutube className='text-3xl text-red-600' /><span className='text-xl font-semibold'>YouTube <sup className="text-[10px]">IN</sup> </span></div>
-            </div>
+export default function Navbar() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const searchTerm = useAppSelector((state) => state.youtubeApp.searchTerm);
 
-            <div className='flex justify-center items-center gap-5'>
-                <form>
-                    <div className='flex bg-zinc-900 items-center h-10 px-4 pr-0 rounded-full'>
-                        <div className='flex gap-5 items-center pr-5'>
-                            <input type='text' placeholder='Search' className='w-96 bg-zinc-900 focus:outline-none'></input>
-                        </div>
-                        <button className='flex items-center text-white h-10 w-16 justify-center bg-zinc-800 rounded-r-3xl'>
-                            <IoIosSearch className='text-xl' /></button>
-                    </div>
-                </form>
-                <div className='bg-[#3c3c3c] rounded-full w-8 h-8'><FaMicrophone className="text-white mt-2 ml-2" /></div>
-            </div>
+  const handleSearch = () => {
+    if (location.pathname !== "/search") navigate("/search");
+    else {
+      dispatch(clearVideos);
+      dispatch(getSearchPageVideos(false));
+    }
+  };
 
-            <div className='flex justify-center items-center gap-9 '>
-                <BiVideoPlus className="text-white text-xl" />
-                <FaRegBell className="text-white text-xl" />
-                <CgProfile className="text-white text-xl" />
-
-            </div>
+  return (
+    <div className="flex justify-between items-center px-14 h-14  bg-[#212121] opacity-95 sticky top-0 z-50">
+      <div className="flex gap-8 items-center text-2xl">
+        <div>
+          <GiHamburgerMenu />
         </div>
-    )
-}
+        <div className="flex gap-1 items-center justify-center">
+          <BsYoutube className="text-3xl text-red-600" />
+          <span className="text-2xl ">Youtube</span>
+        </div>
+      </div>
+      <div className="flex items-center justify-center gap-5">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleSearch();
+          }}
+        >
+          <div className="flex bg-zinc-900 items-center h-10 px-4 pr-0 rounded-3xl">
+            <div className="flex gap-5 items-center pr-5">
+              <input
+                type="text"
+                placeholder="Search"
+                className="w-96 bg-zinc-900 focus:outline-none border-none"
+                value={searchTerm}
+                onChange={e=>dispatch(changeSearchTerm(e.target.value))}
+              />
+            </div>
+            <button className="h-10 w-16 flex items-center justify-center bg-zinc-800 rounded-r-3xl">
+              <AiOutlineSearch className="text-xl " />
+            </button>
+          </div>
+        </form>
 
-export default Navbar
+        <div className="text-xl p-3 bg-zinc-900 rounded-full">
+          <FaMicrophone />
+        </div>
+      </div>
+      <div className="flex gap-8 items-center text-xl">
+        <RiVideoAddLine />
+        <div className="relative">
+          <BsBell />
+          <span className="absolute bottom-2 left-2 text-xs bg-red-600 rounded-full px-1">
+            {" "}
+            9+{" "}
+          </span>
+        </div>
+        <img
+          src="https://yt3.googleusercontent.com/Ad8Q8PblBvMbfVEEpuSiAvcLoI6XqEecc4GEjCN41716Zc1lLEjR6lPB4JXjKzMLVFYskARDaNU=s900-c-k-c0x00ffffff-no-rj"
+          alt="profile logo"
+          className="w-9 h-9 rounded-full"
+        />
+      </div>
+    </div>
+  );
+}
